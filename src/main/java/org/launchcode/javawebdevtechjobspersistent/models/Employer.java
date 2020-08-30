22288f2ffb316605ae0ace86f01dc9dbd110e709
Brian Jenkins <brianjenkins94@gmail.com>
2020-08-30 16:03:20 -0400

    Inital commit-Part 2 including abstract entity and Models

---
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -0,0 +1,42 @@
+package org.launchcode.javawebdevtechjobspersistent.models;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Employer extends AbstractEntity {
+
+    @NotNull(message = "Location is required.")
+    @Size(min = 3, max = 100)
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+
+    public Employer() {}
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+}
